492d8b49cecce69b16bf3abf1b2aaf4542b6866e
alex <aleksandrdenysov38@gmail.com>
2026-02-11 22:45:14 +0200

    Update

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,4 +1,25 @@
 package app;
 
 public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+
+        for (String str : strs) {
+            count++;
+
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+
+            stringBuilder.append(count)
+                    .append(") ")
+                    .append(str)
+                    .append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
+
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,18 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is my app");
+        DataProvider provider = new DataProvider();
+        Corrector corrector = new Corrector();
+
+        String result = corrector.handleData(provider.getData());
+        getOutput(result);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
+
+
